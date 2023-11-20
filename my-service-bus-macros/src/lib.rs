38b038c323acb21aa3dbb6c04ff4d6906117a495
amigin <amigin@gmail.com>
2023-11-20 10:54:16 +0200

    Added my_sb_entity_protobuf_model_with_version

--- my-service-bus-macros/src/lib.rs
+++ my-service-bus-macros/src/lib.rs
@@ -1,10 +1,21 @@
 mod fn_impl_protobuf_model;
-
+mod fn_impl_protobuf_model_with_version;
 use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
 pub fn my_sb_entity_protobuf_model(attr: TokenStream, item: TokenStream) -> TokenStream {
     match crate::fn_impl_protobuf_model::generate(attr, item) {
+        Ok(result) => result,
+        Err(err) => err.into_compile_error().into(),
+    }
+}
+
+#[proc_macro_attribute]
+pub fn my_sb_entity_protobuf_model_with_version(
+    attr: TokenStream,
+    item: TokenStream,
+) -> TokenStream {
+    match crate::fn_impl_protobuf_model_with_version::generate(attr, item) {
         Ok(result) => result,
         Err(err) => err.into_compile_error().into(),
     }
