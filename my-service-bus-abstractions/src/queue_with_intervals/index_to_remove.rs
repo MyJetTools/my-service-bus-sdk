190b4f7e01b0877ccb4b8d14cad7246093377855
amigin <amigin@gmail.com>
2025-02-21 16:43:20 +0200

    Added way more unit tests

--- my-service-bus-abstractions/src/queue_with_intervals/index_to_remove.rs
+++ my-service-bus-abstractions/src/queue_with_intervals/index_to_remove.rs
@@ -1,5 +1,5 @@
 use super::QueueIndexRange;
-
+#[derive(Debug)]
 pub enum IndexToRemoveValue {
     IncLeft(usize),
     DecRight(usize),
@@ -45,5 +45,93 @@ impl IndexToRemoveValue {
         }
 
         Self::NoValue
+    }
+
+    pub fn is_no_value(&self) -> bool {
+        match self {
+            IndexToRemoveValue::NoValue => true,
+            _ => false,
+        }
+    }
+
+    pub fn unwrap_as_inc_left(&self) -> usize {
+        match self {
+            IndexToRemoveValue::IncLeft(index) => *index,
+            _ => panic!("{:?}", self),
+        }
+    }
+
+    pub fn unwrap_as_dec_right(&self) -> usize {
+        match self {
+            IndexToRemoveValue::DecRight(index) => *index,
+            _ => panic!("{:?}", self),
+        }
+    }
+
+    pub fn unwrap_as_remove(&self) -> usize {
+        match self {
+            IndexToRemoveValue::Remove(index) => *index,
+            _ => panic!("{:?}", self),
+        }
+    }
+
+    pub fn unwrap_as_split(&self) -> (usize, &QueueIndexRange, &QueueIndexRange) {
+        match self {
+            IndexToRemoveValue::Split { index, left, right } => (*index, left, right),
+            _ => panic!("{:?}", self),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::queue_with_intervals::{IndexToRemoveValue, QueueIndexRange};
+
+    #[test]
+    fn test_options() {
+        let intervals = vec![
+            QueueIndexRange {
+                from_id: 10,
+                to_id: 20,
+            },
+            QueueIndexRange {
+                from_id: 22,
+                to_id: 30,
+            },
+            QueueIndexRange {
+                from_id: 40,
+                to_id: 50,
+            },
+            QueueIndexRange {
+                from_id: 55,
+                to_id: 55,
+            },
+        ];
+
+        assert_eq!(IndexToRemoveValue::new(&intervals, 5).is_no_value(), true);
+
+        assert_eq!(
+            IndexToRemoveValue::new(&intervals, 10).unwrap_as_inc_left(),
+            0
+        );
+
+        let result = IndexToRemoveValue::new(&intervals, 11);
+        let result = result.unwrap_as_split();
+        assert_eq!(result.0, 0);
+        assert_eq!(result.1.from_id, 10);
+        assert_eq!(result.1.to_id, 10);
+
+        assert_eq!(result.2.from_id, 12);
+        assert_eq!(result.2.to_id, 20);
+
+        assert_eq!(
+            IndexToRemoveValue::new(&intervals, 20).unwrap_as_dec_right(),
+            0
+        );
+
+        assert_eq!(
+            IndexToRemoveValue::new(&intervals, 55).unwrap_as_remove(),
+            3
+        );
     }
 }
